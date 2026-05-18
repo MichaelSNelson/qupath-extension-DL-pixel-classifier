42967f2a3af4f53a764f657fefd932602ab3ae11
MichaelSNelson <imagescientistwebsite@gmail.com>
2026-05-18 11:35:53 -0500

    Bump version to 0.7.11-dev

--- src/main/java/qupath/ext/dlclassifier/service/ApposeService.java
+++ src/main/java/qupath/ext/dlclassifier/service/ApposeService.java
@@ -843,7 +843,7 @@ public class ApposeService {
      * </ol>
      */
     /** Extension version. Used for pip URL construction and script generation. */
-    public static final String DL_SERVER_VERSION = "0.7.10";
+    public static final String DL_SERVER_VERSION = "0.7.11-dev";
 
     private static final boolean IS_DEV_BUILD = DL_SERVER_VERSION.contains("-dev");
     private static final String DL_SERVER_PIP_URL;
